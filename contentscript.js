55eb9922527d3b78bddda74c2497ad89432bc6d4
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-06-18 23:46:50 -0400

    v0.3

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,17 @@
-const libcheat = document.createElement('script');
-libcheat.src = chrome.extension.getURL('libcheat.js');
-(document.head || document.documentElement).appendChild(libcheat);
 
-const s = document.createElement('script');
-s.src = chrome.extension.getURL('cheats/calc.js');
-(document.head || document.documentElement).appendChild(s);
+const configURL = chrome.extension.getURL('cheats/config.json');
+const xmlHttp = new XMLHttpRequest();
+xmlHttp.open("GET", configURL, false); // false for synchronous request
+xmlHttp.send(null);
+const config = JSON.parse(xmlHttp.responseText);
+
+
+if(config[location.href]) {
+	const libcheat = document.createElement('script');
+	libcheat.src = chrome.extension.getURL('libcheat.js');
+	(document.head || document.documentElement).appendChild(libcheat);
+
+	const s = document.createElement('script');
+	s.src = chrome.extension.getURL('cheats/'+config[location.href]);
+	(document.head || document.documentElement).appendChild(s);
+}
