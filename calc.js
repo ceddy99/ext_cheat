71f38892f994cc0ded6def57ad296242e951eb44
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-06-18 23:24:15 -0400

    ext_cheat v0.1 !!!

---
+++ calc.js
@@ -0,0 +1,225 @@
+
+
+const uidivid = 'cheat_ui_div';
+(function() {
+	if(document.getElementById(uidivid)) return;
+	const div = document.createElement('div');
+	div.id = uidivid;
+	div.style="border-width:3px;background-color:#333;border-style:solid;position:fixed;padding:7px;bottom:0;right:0;max-width:450px;z-index:999"
+	document.body.appendChild(div);
+})();
+
+let tasks = [];
+
+setInterval(()=>tasks.forEach(task => task()), 50);
+
+const libcheat = {
+	addTask: (task, toggleable=false, label="") => {
+		if(toggleable) {
+			const div = document.getElementById(uidivid);
+			const box = document.createElement('input');
+			const text = document.createElement('span');
+			text.style.color = '#66FF33';
+			text.innerText = label;
+			box.setAttribute("type", "checkbox");
+			div.appendChild(box);
+			div.appendChild(text);
+			div.appendChild(document.createElement('br'));
+			const oldtask = task;
+			task = () => {if(box.checked) oldtask()}
+		}
+		tasks.push(task);
+	},
+	clearTasks: () => tasks = [],
+	addButton: (onClick, label) => {
+		const div = document.getElementById(uidivid);
+		const button = document.createElement('button');
+		button.innerText = label;
+		button.addEventListener('click', onClick);
+		div.appendChild(button);
+		div.appendChild(document.createElement('br'));
+	},
+	watch: (watcher) => {
+		const div = document.getElementById(uidivid);
+		const text = document.createElement('span');
+		text.style.color = '#66FF33';
+		text.innerText = "pending update";
+		div.appendChild(text);
+		div.appendChild(document.createElement('br'));
+		tasks.push(()=> text.innerText = watcher());
+	},
+	paramTask: (task, label) => {
+		const div = document.getElementById(uidivid);
+		const text = document.createElement('span');
+		text.style.color = '#66FF33';
+		text.innerText = label;
+		div.appendChild(text);
+		const input = document.createElement('input');
+		div.appendChild(input);
+		div.appendChild(document.createElement('br'));
+		tasks.push(()=> task(input.value));
+	}
+}
+
+
+libcheat.addTask(()=>[0, 1, 2, 3, 4, 5, 6, 7].forEach(x => researchBuy(x)), true, 'auto research');
+libcheat.addTask(()=>[0, 1, 2, 3, 4, 5].forEach(x => shopBuy(x)), true, 'auto shop');
+
+function atMaxBase() {
+	return game.base.eq(calcMaxBase());
+}
+
+function atMaxDigit() {
+	return game.digits.eq(calcMaxDigit());
+}
+
+function atMaxNumber() {
+	return game.base.pow(game.digits).sub(game.number).lessThanOrEqualTo(1);
+}
+
+function safeReboot() {
+	if(!game.programActive[4]) activeProgram(4);
+	reboot();
+}
+
+function forceRespect() {
+	for(let i = 0; i < 10; i++) quantumUpgradeRespec();
+}
+
+function convertIdx(x) {
+	for(let i = 0; i < 100; i++) {
+		const idx = (Math.floor(i/qUpgradeData.row)+1) + (i%qUpgradeData.col+1)*10;
+		if(idx===x) return i;
+	}
+	throw new Error('wtf' +x);
+}
+
+function autoSetup(setups) {
+	let leastWaste = 99999999;
+	let theSetup = [];
+	for(const setup of setups) {
+		let cost = 0;
+		for(const upg of setup) {
+			cost += getQuantumUpgradeCost(convertIdx(upg));
+		}
+		if(game.qubit.sub(cost).lt(0)) break;
+		theSetup = setup;
+	}
+
+	forceRespect();
+	for(const upg of theSetup) {
+		buyQuantumUpgrade(convertIdx(upg));
+	}
+}
+
+function replaceSing(i) {
+	const positions = [
+		[1, 0],
+		[2, 1],
+		[2, 2],
+		[0, 2],
+		[1, 0],
+		[2, 1],
+		[2, 2],
+		[0, 2],
+		[1, 0],
+		[2, 1],
+	];
+
+	for(const pos of positions) {
+		singularityGridClick(pos[0], pos[1], 'r');
+	}
+	selectedMachine = i;
+	const m = Math.min(positions.length, getSingularityMachineHave(machineIdx[i]));
+	for(let i = 0; i < m; i++) {
+		singularityGridClick(positions[i][0], positions[i][1], 'l');
+	}
+
+	game.singularityGrid[21].rotate = 1;
+	game.singularityGrid[22].rotate = 1;
+	game.singularityGrid["02"].rotate = 3;
+}
+
+libcheat.addTask(function() {
+	if(calcMultiProcess()<5) {
+		if(!game.programActive[0]) activeProgram(0);
+		if(atMaxNumber() && !atMaxDigit() && !game.programActive[2]) activeProgram(2);
+		if(atMaxDigit() && !atMaxBase() && !game.programActive[3]) activeProgram(3);
+		if(atMaxDigit() && atMaxBase() && atMaxNumber() && !game.programActive[1]) activeProgram(1);
+		if(game.money.mul(1e5).lt(calcMoneyGain()) && !game.programActive[1]) activeProgram(1);
+	} else {
+		const order = [0, 1, 2, 3, 4, 6, 5];
+		for(let i = 0; i < Math.min(order.length, calcMultiProcess()); i++) {
+			if(!game.programActive[order[i]]) activeProgram(order[i]);
+		}
+	}
+
+	game.base.pow(game.digits).sub(game.number).lessThanOrEqualTo(1)
+}, true, 'smart process');
+
+libcheat.addTask(function() {
+	quantum()
+}, true, 'smart quantum');
+libcheat.addTask(function() {
+	singularity()
+}, true, 'auto singularity');
+
+libcheat.addTask(function() {
+	const rp = game.researchPoint.eq(0)?D(1):game.researchPoint
+	if(rp.mul(1e5).lt(calcRPGain())) safeReboot();
+	if(game.durability.lt(0.5)) safeReboot();
+}, true, 'smart reboot');
+
+libcheat.addButton(safeReboot, 'reboot');
+
+libcheat.addButton(forceRespect, 'respect');
+
+libcheat.addButton(function() {
+	forceRespect();
+	const extra = [35, 34, 33, 32, 31, 21, 11, 41, 51, 52, 42, 22, 12, 53, 43, 23, 13, 54, 44, 24, 14, 55, 45, 25, 15, 46, 56, 36, 26, 16, 71];
+	for(const upg of extra) buyQuantumUpgrade(convertIdx(upg));
+}, 'qubits');
+
+
+
+const qlabsSetups = [
+	[32, 23, 22, 21, 11],
+	[32, 21, 11, 13, 23, 22],
+	[32, 22, 21, 11, 12, 13, 23],
+	[32, 22, 21, 11, 12, 13, 23, 24],
+	[21, 11, 12, 22, 32, 23, 25, 13],
+	[25, 23, 13, 12, 32, 22, 21, 11],
+	[32, 22, 21, 11, 12, 25, 23, 13, 24],
+	[25, 15, 23, 13, 12, 11, 21, 22, 32],
+	[25, 15, 23, 13, 12, 11, 21, 22, 32],
+	[15, 25, 32, 22, 21, 11, 12, 13, 23, 24],
+	[26, 23, 22, 32, 21, 11, 12],
+	[26, 32, 22, 21, 11, 12, 13, 23],
+	[26, 25, 32, 22, 21, 11],
+	[26, 25, 32, 22, 21, 11, 12],
+	[26, 25, 23, 22, 32, 21, 11, 12],
+	[26, 25, 32, 22, 21, 11, 12, 23, 13],
+	[26, 25, 32, 22, 21, 11, 12, 13, 23, 24],
+	[26, 25, 15, 13, 23, 22, 12, 11, 21, 32],
+	[26, 25, 15, 32, 22, 21, 11, 12, 13, 23, 24, 14],
+	[16, 26, 25, 32, 22, 21, 11, 12, 13, 23],
+	[16, 26, 25, 14, 13, 23, 22, 12, 11, 21, 32],
+	[16, 26, 25, 15, 13, 23, 12, 11, 21, 22, 32],
+	[16, 26, 25, 15, 32, 22, 21, 11, 12, 13, 23, 14],
+	[26, 16, 15, 25, 24, 14, 13, 23, 22, 12, 11, 21, 32],
+	[36, 26, 16, 15, 25, 32, 22, 21, 11, 12, 23, 13],
+	[36, 26, 16, 15, 25, 24, 14, 13, 23, 22, 12, 11, 21, 32],
+];
+
+libcheat.addButton(function() {
+	autoSetup(qlabsSetups);
+	const extra = [42, 43, 44, 45, 35, 34, 33, 31, 41, 51, 52, 53, 54, 55, 46, 56, 71, 37, 27, 17, 75];
+	for(const upg of extra) {
+		buyQuantumUpgrade(convertIdx(upg));
+	}
+}, 'qlabs');
+
+const boosts = ["base","digit","money","rp","rspeed","speed", 'qubits'];
+for(let i = 0; i < boosts.length; i++) {
+	libcheat.addButton(()=> replaceSing(i), boosts[i]);
+}
