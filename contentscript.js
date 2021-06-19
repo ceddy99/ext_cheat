77c222d3bbabca193bf809d0f1bf010376d4b538
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-06-18 23:33:04 -0400

    v0.2

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,7 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('calc.js');
+const libcheat = document.createElement('script');
+libcheat.src = chrome.extension.getURL('libcheat.js');
+(document.head || document.documentElement).appendChild(libcheat);
+
+const s = document.createElement('script');
+s.src = chrome.extension.getURL('cheats/calc.js');
 (document.head || document.documentElement).appendChild(s);
---
+++ libcheat.js
@@ -0,0 +1,60 @@
+const uidivid = 'cheat_ui_div';
+(function() {
+	if(document.getElementById(uidivid)) return;
+	const div = document.createElement('div');
+	div.id = uidivid;
+	div.style="border-width:3px;background-color:#333;border-style:solid;position:fixed;padding:7px;bottom:0;right:0;max-width:450px;z-index:999"
+	document.body.appendChild(div);
+})();
+
+let tasks = [];
+
+setInterval(()=>tasks.forEach(task => task()), 50);
+
+const libcheat = {
+	addTask: (task, toggleable=false, label="") => {
+		if(toggleable) {
+			const div = document.getElementById(uidivid);
+			const box = document.createElement('input');
+			const text = document.createElement('span');
+			text.style.color = '#66FF33';
+			text.innerText = label;
+			box.setAttribute("type", "checkbox");
+			div.appendChild(box);
+			div.appendChild(text);
+			div.appendChild(document.createElement('br'));
+			const oldtask = task;
+			task = () => {if(box.checked) oldtask()}
+		}
+		tasks.push(task);
+	},
+	clearTasks: () => tasks = [],
+	addButton: (onClick, label) => {
+		const div = document.getElementById(uidivid);
+		const button = document.createElement('button');
+		button.innerText = label;
+		button.addEventListener('click', onClick);
+		div.appendChild(button);
+		div.appendChild(document.createElement('br'));
+	},
+	watch: (watcher) => {
+		const div = document.getElementById(uidivid);
+		const text = document.createElement('span');
+		text.style.color = '#66FF33';
+		text.innerText = "pending update";
+		div.appendChild(text);
+		div.appendChild(document.createElement('br'));
+		tasks.push(()=> text.innerText = watcher());
+	},
+	paramTask: (task, label) => {
+		const div = document.getElementById(uidivid);
+		const text = document.createElement('span');
+		text.style.color = '#66FF33';
+		text.innerText = label;
+		div.appendChild(text);
+		const input = document.createElement('input');
+		div.appendChild(input);
+		div.appendChild(document.createElement('br'));
+		tasks.push(()=> task(input.value));
+	}
+}
